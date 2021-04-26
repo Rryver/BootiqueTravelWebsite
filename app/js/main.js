0414aea92276a26985a7f9802383de3a84dacdb8
Alexandr Kolosov <ryver.rainege@gmail.com>
2021-04-26 17:58:16 +0400

    save

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,9 @@
-$(function () {
-
-})
+// window.onload = function () {
+// //     let showcardheight = document.querySelector('.showcard-travel__comment').offsetHeight +
+// //         document.querySelector('.showcard-travel__body').offsetHeight;
+// //
+// //     let heightToAdd = document.querySelector('.showcards__showcard-section').offsetHeight +
+// //         (showcardheight - document.querySelector('.showcards__container').offsetHeight);
+// //
+// //     document.querySelector('.showcards__container').style.gridTemplateRows = "252px " + heightToAdd + "px";
+// // };
